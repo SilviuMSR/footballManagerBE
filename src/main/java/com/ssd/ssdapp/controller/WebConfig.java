e135dae354bf00b48ed6f56197450859b7047231
Silviu <manzursilviU@yahoo.com>
2019-01-09 17:42:59 +0600

    test commit2

--- src/main/java/com/ssd/ssdapp/controller/WebConfig.java
+++ src/main/java/com/ssd/ssdapp/controller/WebConfig.java
@@ -11,6 +11,6 @@ public class WebConfig implements WebMvcConfigurer {
  
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("https://ssdproject-4a4a4.firebaseapp.com");
+        registry.addMapping("/**");
     }
 }
